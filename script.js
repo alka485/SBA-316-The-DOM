2fe71114e83560d63313fffa239861a5dcf3527d
Alka Sah <alkasah485@gmail.com>
2024-05-19 02:37:05 -0700

    modification in script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+//items
+const menu = [
+    {
+        id: 1,
+        title: "Buttermilk Pancake",
+        category: "Breakfast",
+        price: $15,
+        img: "./images/item-1.jpeg",
+        desc:"I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed"
+    },
+    {
+        id: 2,
+        title: "Diner Double",
+        category: "Breakfast",
+        price: $13,
+        img: "./images/item-2.jpeg",
+        desc:"vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats"
+
+    }
+]
